b050b182a2db13765c1ae02864c32a16345427c2
chinglamn1234 <ngchinglamn@gmail.com>
2026-01-15 17:20:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
+    "Im getting anngry",
+    "EDDY WONG!",
+    "STOP PRESSING NO !",
+    "OK,I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Just kidding, say yes !❤️"
 ];
 
 let messageIndex = 0;
